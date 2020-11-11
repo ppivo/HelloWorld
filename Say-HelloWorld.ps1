d8e11b1566e45f0ac2029cd90ad1957ef8616a67
Petr Pivoda <54377798+ppivo@users.noreply.github.com>
2020-11-11 01:22:25 +0100

    added OS dependent code
    this script works different way whether running on linux or windows

--- Say-HelloWorld.ps1
+++ Say-HelloWorld.ps1
@@ -2,7 +2,19 @@
 "Hello $(whoami)!"
 "------------"
 "Your local time is $(Get-Date)"
-"Your machine's hostname is $($env:computername) "
-"Your IP is $((Get-NetIPAddress -AddressFamily ipv4 -PrefixOrigin DHCP).ipaddress)"
-"Your DNS domains are:`n $((Get-Wmiobject Win32_NetworkAdapterConfiguration |? dnsdomain -ne $null).dnsdomain)" 
+"Your machine's hostname is $(hostname)"
+$OSplatform= [System.Environment]::OSVersion.Platform
+"Your Operating System platform is $($OSplatform)"
+
+if ($OSplatform -eq "Win32NT")
+{
+    "Your IP is $((Get-NetIPAddress -AddressFamily ipv4 -PrefixOrigin DHCP).ipaddress)"
+    "Your DNS domains are: $((Get-Wmiobject Win32_NetworkAdapterConfiguration |Where-Object dnsdomain -ne $null).dnsdomain)" 
+    "Local users of your machine are: $((get-localuser |Where-Object enabled -eq $true).name)"
+}else
+   {
+    Your distro details are: $(uname -a)""
+    "More details in progress... Stand by!"
+   }
+
 "============"
